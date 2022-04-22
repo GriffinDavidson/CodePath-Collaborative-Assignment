989add994449bf7e8456ea38adb8e03a44f7dae6
GriffinDavidson <griffindavidson1@icloud.com>
2022-04-22 12:19:49 -0700

    added MapView

---
+++ GymPal/Models/Gyms.swift
@@ -0,0 +1,17 @@
+import Foundation
+import CoreLocation
+
+
+struct Gyms {
+    
+
+//    var imageURL: URL?
+//    var url: URL?
+    var name: String?
+   // var coordinates: [String]?
+    var longitude: Double?
+    var latitude: Double?
+    var address: String?
+    var coordinates: CLLocationCoordinate2D?
+    
+}
--- GymPal/ViewControllers/HomeViewController.swift
+++ GymPal/ViewControllers/HomeViewController.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
+import MapKit
 
-class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate
+class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, MKMapViewDelegate
 {
     
     @IBOutlet weak var peopleCollectionView: UICollectionView!
+    @IBOutlet weak var map: MKMapView!
+    
+    fileprivate let locationManager: CLLocationManager = CLLocationManager()
+    let defaultLocation = CLLocation(latitude: 37.773972, longitude: -122.431297)
+    let defaultLocationCoordinate = CLLocationCoordinate2D(latitude: 37.773972, longitude: -122.431297)
+    
+    var gymArray: [Gyms] = []
+    var gymPins = [MKPointAnnotation]()
     
     var users = [User]()
     {
@@ -47,6 +56,21 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
 //
 //        let width = (view.frame.width - layout.minimumInteritemSpacing * 2) / 2
 //        layout.itemSize = CGSize(width: width, height: 3 / 2 * width)
+        
+        map.delegate = self
+        
+        let center: CLLocationCoordinate2D? = nil
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center ?? defaultLocationCoordinate, span: span)
+        map.setRegion(region, animated: true)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        findCurrentLocation()
+        centerMap()
+        loadGyms()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
@@ -91,5 +115,105 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func findCurrentLocation() {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.distanceFilter = kCLDistanceFilterNone
+            locationManager.startUpdatingLocation()
+          //  gymMapView.showsUserLocation = true
+            
+            
+        }
+    
+    func centerMap() {
+            
+            let location = locationManager.location
+            let defaultLat = 37.773972
+            let defaultLong = -122.431297
+            let defaultCoordinate = CLLocationCoordinate2D(latitude: defaultLat, longitude: defaultLong)
+            let span = MKCoordinateSpan(latitudeDelta: 0.04, longitudeDelta: 0.04)
+            let mapRegion = MKCoordinateRegion(center: location?.coordinate ?? defaultCoordinate, span: span)
+            map.setRegion(mapRegion, animated: true)
+        }
+    
+    func loadGyms() {
 
+    //        if locationManager.location == nil {
+    //            print("cannot pull location")
+    //            let location = CLLocationCoordinate2D(latitude: 37.773972, longitude: -122.431297)
+    //        }
+            let location = locationManager.location ?? defaultLocation
+            print("Loading gyms")
+            retrieveGyms(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude, category: "gyms", limit: 20, sortBy: "distance", locale: "en_US") { (response, error) in
+                if let response = response {
+                    print("received a response")
+                    print(response)
+                    
+                    DispatchQueue.main.async {
+                        self.map.addAnnotations(self.gymPins)
+                        print("Dispatching gym pins: \(self.gymPins)")
+                    }
+                }
+                    
+                
+                
+            }
+        }
+    
+    func retrieveGyms(latitude: Double, longitude: Double, category: String, limit: Int, sortBy: String, locale: String, completionHanlder: @escaping ([Gyms]?, Error?) -> Void) {
+                
+            let apikey = "some key"
+            
+            let baseURL = "https://api.yelp.com/v3/businesses/search?latitude=\(latitude)&longitude=\(longitude)&categories=\(category)&limit=\(limit)&sort_by=\(sortBy)&locale=\(locale)"
+                       //  "https://api.yelp.com/v3/businesses/search?latitude=\(latitude)&longitude=\(longitude)&categories=\(category)&limit=\(limit)&sort_by=\(sortBy)&locale=\(locale)"
+                
+                let url = URL(string: baseURL)
+                var request = URLRequest(url:url!)
+                request.setValue("Bearer \(apikey)", forHTTPHeaderField: "Authorization")
+                request.httpMethod = "GET"
+
+                URLSession.shared.dataTask(with: request) { (data, response, error) in
+                    if let error = error {
+                        completionHanlder(nil, error)
+                        print("error loading: \(error.localizedDescription)")
+                    }
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options:[])
+                        guard let resp = json as? NSDictionary else { return }
+
+                        guard let businesses = resp.value(forKey: "businesses") as? [NSDictionary] else { return }
+                        print("returning businesses")
+                        //print(businesses)
+                        var gymsList: [Gyms] = []
+                        self.gymPins = []
+
+                        for business in businesses {
+                            var gyms = Gyms()
+                            
+                            let annotation = MKPointAnnotation()
+                            gyms.name = business.value(forKey: "name") as? String
+                            //gyms.coordinates = business.value(forKey: "coordinates") as? [String]
+                            gyms.longitude = business.value(forKeyPath: "coordinates.longitude") as? Double
+                            gyms.latitude = business.value(forKeyPath: "coordinates.latitude") as? Double
+                            let address = business.value(forKeyPath: "location.display_address") as? [String]
+                            gyms.address = address?.joined(separator: "\n")
+                            let coordinates = CLLocationCoordinate2D(latitude: gyms.latitude!, longitude: gyms.longitude!)
+                            gyms.coordinates = coordinates
+                            annotation.coordinate = coordinates
+                            annotation.title = gyms.name
+                            gymsList.append(gyms)
+                            self.gymPins.append(annotation)
+                            print(self.gymPins)
+                        }
+                        
+                        completionHanlder(gymsList, nil)
+                        
+                    } catch {
+                        print("Caught Error: \(error.localizedDescription)")
+                    }
+                    
+                } .resume()
+                
+            }
 }
